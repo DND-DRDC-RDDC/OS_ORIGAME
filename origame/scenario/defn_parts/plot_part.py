97d826ba3fef973d872aed01bb28397efa057b2b
Esraa Abujayyab <esraa.abujayyab@larus.com>
2023-03-14 12:02:31 -0400

    Revert "updating a plot now works after resolving an error in the plot script"
    This reverts commit 141751c2b1fb17bb827ab14789ed8e201b2e99b1.

--- origame/scenario/defn_parts/plot_part.py
+++ origame/scenario/defn_parts/plot_part.py
@@ -415,7 +415,7 @@ class PlotPart(BasePart, PyScriptExec, IScriptedPart):
         already, first compile and execute script. Any exception raised in the script will get trapped and
         put in the part's last_exec_err_info property. No exceptions should escape from this method.
         """
-        self._clear_own_alerts(ScenAlertLevelEnum.error, ErrorCatEnum.compile, ErrorCatEnum.config_plot, ErrorCatEnum.axes,  ErrorCatEnum.plot)
+        self._clear_own_alerts(ScenAlertLevelEnum.error, ErrorCatEnum.compile, ErrorCatEnum.config_plot, ErrorCatEnum.axes)
         try:
             alert_categ = ErrorCatEnum.compile
             self._check_compile_and_exec()
@@ -478,6 +478,7 @@ class PlotPart(BasePart, PyScriptExec, IScriptedPart):
 
         assert not self.has_alerts()
 
+        self._clear_own_alerts(ScenAlertLevelEnum.error, ErrorCatEnum.plot)
         try:
             self._py_exec(self.__script_update_fig)
             self.__plot_update_reqd__possible = False
