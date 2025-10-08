44df228125ba6d6af7103fee61831c47ccd131dc
LynneSerre <67338662+LynneSerre@users.noreply.github.com>
2025-10-08 10:05:29 -0400

    Update batch_sim_manager.py
    Potential fix for batch mode issues, simply unpack output from load method of ScenarioManager (taken from Version 0.8.0)

--- origame/batch_sim/batch_sim_manager.py
+++ origame/batch_sim/batch_sim_manager.py
@@ -377,7 +377,7 @@ class _BsmStateRunning(BaseFsmState, CapturesNextReadyScenPath):
 
         scen_mgr = ScenarioManager()
         try:
-            scen = scen_mgr.load(self._batch_scen_path)
+            scen, _ = scen_mgr.load(self._batch_scen_path)
         except Exception as exc:
             log.error("Could not load scenario to process batch data: {}", exc)
             return
@@ -1337,3 +1337,4 @@ class BatchMonitor:
             self.__num_cores_actual = replics_left
             if replics_left:
                 log.debug('Batch now using {} cores (1 replication process/core)', self.__num_cores_actual)
+
